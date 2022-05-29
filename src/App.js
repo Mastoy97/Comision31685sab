fe06bd53f15eb32ab28ec994e3e1181ca8734c35
Gonzalo Mastoy <mastoy97@gmail.com>
2022-05-28 23:47:31 -0300

    2clase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar/>
+      <ItemListContainer greeting= "Cultura"/>
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <button> Libros </button>
+            <button> Peliculas</button>
+            <h3>Cultura</h3>
+        </nav>     
+
+    )
+    
+}
+
+export default NavBar
